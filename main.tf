24c67540d800e20b7fa714b70bbdaf705a2289c1
JodyPetroni <jpetroni1@bigpond.com>
2020-10-09 09:06:44 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "terrastatestoragejmp"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
 module "rg" {
   source      = "./src"
 }
