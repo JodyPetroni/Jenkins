a98c0c2e6bec34bde8895709a2ad572027b91bbb
JodyPetroni <jpetroni1@bigpond.com>
2020-10-26 13:09:43 +1100

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -83,7 +83,7 @@ resource "azurerm_iothub" "example" {
     batch_frequency_in_seconds = 60
     max_chunk_size_in_bytes    = 10485760
     container_name             = "testc"
-    encoding                   = "json"
+    encoding                   = "Avro"
     file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"
   }
 }
